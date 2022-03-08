8a5fdb30968ad1aac350e1d3b7c8b6031c10207b
CesarCruz-lab <rbcesar86@gmail.com>
2022-03-08 13:06:14 -0500

    testes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+
+const Page: React.FC<{ title: string }> = ({ title }) => (
+  <div>
+    <h1>{title}</h1>
+  </div>
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Page title="Home Page" />} />
+          <Route path="page2" element={<Page title="Page 2" />} />
+          <Route path="page3">
+            <Route path="" element={<Page title="SubPage" />} />
+            <Route path="subpage1" element={<Page title="Subpage1"></Page>} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
